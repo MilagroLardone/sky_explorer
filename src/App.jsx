d63e59addb8b740e7ed8c8339903a1c0c22df088
MilagroLardone <milagrolardone@gmail.com>
2025-04-29 18:56:00 -0300

    footer
    Refactor footer layout and style

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import Header from "./components/Header/Header.jsx";
 import Footer from "./components/Footer/Footer.jsx";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 function App() {
   return (
