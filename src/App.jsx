e6010a3302529dbfb479c0ef9b62c3fed8469cfd
Tiziano <tizianotosatto2005@gmail.com>
2025-04-30 11:31:50 -0300

    Correccion de Rutas

--- src/App.jsx
+++ src/App.jsx
@@ -2,42 +2,14 @@ import "./App.css";
 import Header from "./components/Header/Header.jsx";
 import Footer from "./components/Footer/Footer.jsx";
 import "@fortawesome/fontawesome-free/css/all.min.css";
+import Home from "./pages/Home/Home.jsx";
 
 function App() {
-  const offers = [
-    {
-      id: 1,
-      image: bariloche,
-      destination: "Bariloche",
-      origin: "Buenos Aires",
-      price: "1450000",
-    },
-    {
-      id: 2,
-      image: mendoza,
-      destination: "Mendoza",
-      origin: "Buenos Aires",
-      price: "650000",
-    },
-  ];
 
   return (
     <div className="app">
       <Header />
-      <main className="main-content">
-        <SearchBar />
-        <div className="offers-container">
-          {offers.map((offer) => (
-            <OfferBox
-              key={offer.id}
-              image={offer.image}
-              destination={offer.destination}
-              origin={offer.origin}
-              price={offer.price}
-            />
-          ))}
-        </div>
-      </main>
+      <Home/>
       <Footer />
     </div>
   );
